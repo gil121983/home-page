55fd9ec95774ab0866544cd69bc79bb155d430ec
gil121983 <gilstolar@icloud.com>
2020-03-07 04:38:06 +0200

    2nd commit

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-let cityName = "Kfar Saba";
-let countryName = "Israel";
 const apikey = key;
+let cityName = "";
 let hour;
 let fakeHour = 0;
-const userName = 'Gil';
+let userName = "";
+let testState = "OFF"
+
+init(openSettingDialog)
+
+function init(callbackSettings) {
+    createSettingsBtn()
+    return (cityName) ? render() : callbackSettings();
+}
+
+function render() {
+    fetchWeather(cityName, apikey)
+    showTime()
+    if (testState === "ON")
+        testClock()
+    else
+        setColorByHour(hour)
+}
 
 function greet(userName, hour) {
     if (hour >= 5 && hour <= 14)
@@ -19,7 +35,7 @@ function greet(userName, hour) {
 
 function showTime() {
     let currentTime = new Date()
-    hour = currentTime.getHours();
+    let hour = currentTime.getHours();
     let min = (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes()
     document.querySelector('.clock').innerHTML = `${hour}:${min}`
 
@@ -32,6 +48,7 @@ function showTime() {
     let dayOfMonth = currentTime.getDate()
     let monthName = monthNames[currentTime.getMonth()]
     document.querySelector('.dayNdate').innerHTML = `${dayName} ${dayOfMonth} ${monthName}`;
+    greet(userName,hour)
     setTimeout(() => showTime(), 1000);
 };
 
@@ -43,17 +60,17 @@ function weatherStyle(data) {
         document.querySelector('.clouds').setAttribute('style', 'visibility:visible')
 
     if (data.rain)
-        document.querySelector('.rain').setAttribute('style','visibility:visible')
+        document.querySelector('.rain').setAttribute('style', 'visibility:visible')
 
     if (data.snow)
-        document.querySelector('.snow').setAttribute('style','visibility:visible')
+        document.querySelector('.snow').setAttribute('style', 'visibility:visible')
 
 
 
 }
 
-function fetchWeather(city, country, apikey) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apikey}`)
+function fetchWeather(city, apikey, ) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
         .then((response) => {
             return response.json();
         })
@@ -64,6 +81,7 @@ function fetchWeather(city, country, apikey) {
             console.log(data)
             weatherStyle(data)
         });
+
 };
 
 function setColorByHour(hour) {
@@ -126,20 +144,150 @@ function setColorByHour(hour) {
     setTimeout(() => setColorByHour(), 600000);
 }
 
-
-fetchWeather(cityName, countryName, apikey)
-showTime()
-testClock()
-
 function testClock() {
     if (fakeHour === 24) {
         fakeHour = 0;
     }
     setTimeout(() => {
+        if(testState==="ON"){
         setColorByHour(fakeHour)
         greet(userName, fakeHour)
+        console.log("test ON - " + fakeHour);
         fakeHour++
         testClock()
-    }, 5000);
+        }
+    }, 2000);
+
 }
 
+function createSettingsBtn() {
+    const setBtnElement = document.createElement('i')
+    setBtnElement.id = "cog-btn";
+    setBtnElement.style = "font-size:50px";
+    setBtnElement.className = "cog-icon fa";
+    setBtnElement.innerHTML = "&#xf013"
+    setBtnElement.onclick = () => {
+        setBtnElement.innerHTML = ""
+        setBtnElement.className = "fa fa-gear fa-spin";
+        openSettingDialog()
+    }
+    document.querySelector('.cog-icon').appendChild(setBtnElement)
+
+}
+
+
+
+
+function openSettingDialog() {
+    const dialogBox = document.querySelector('.dialog-box')
+    dialogBox.setAttribute('style', 'height:600px')
+
+    const dialogElement = document.createElement('div')
+    dialogElement.className = "dialog-element";
+    dialogElement.setAttribute('style', 'height:600px')
+    dialogElement.innerHTML = 'SETTINGS';
+
+    const userNameTitle = document.createElement('p')
+    userNameTitle.innerHTML = 'USER NAME';
+
+    const nameInputElement = document.createElement('input')
+    nameInputElement.placeholder = `${userName}`
+    nameInputElement.className = 'text-input';
+
+    const cityNameTitle = document.createElement('p')
+    cityNameTitle.innerHTML = 'LOCATION';
+
+    const cityInputElement = document.createElement('input')
+    cityInputElement.className = 'text-input';
+    if (cityName)
+    cityInputElement.placeholder = `${cityName},`;
+    else
+    cityInputElement.placeholder = 'City Name, State (opt), Country (opt)'
+
+    const imgTitle = document.createElement('p')
+    imgTitle.innerHTML = 'CHANGE COVER';
+
+    const imgInputElement = document.createElement('input')
+    imgInputElement.placeholder = `Enter cover image URL`
+    imgInputElement.className = 'text-input';
+
+
+
+    const switchBox = document.createElement('div')
+    switchBox.className = 'switch-box';
+
+    const testTitle = document.createElement('p')
+    testTitle.innerHTML = 'TESTER'
+
+    const testSwitchContainer = document.createElement('div')
+    testSwitchContainer.className = 'test-switch-container'
+    const testSwitchElement = document.createElement('div')
+    testSwitchElement.className = 'test-switch'
+    if (testState === "ON") {
+        testSwitchElement.setAttribute('style', 'left:25px')
+        testSwitchContainer.setAttribute('style', ' background:yellowgreen')
+    } else {
+        testSwitchElement.setAttribute('style', 'left:0px')
+        testSwitchContainer.setAttribute('style', ' background:rgb(40,40,40)')
+    }
+    testSwitchContainer.onclick = () => {
+        if (testState === "OFF") {
+            testSwitchElement.setAttribute('style', 'left:25px')
+            testSwitchContainer.setAttribute('style', ' background:yellowgreen')
+            testState = "ON";
+        } else {
+            testSwitchElement.setAttribute('style', 'left:0px')
+            testState = "OFF";
+            testSwitchContainer.setAttribute('style', ' background:rgb(40,40,40)')
+        }
+    }
+    testSwitchContainer.appendChild(testSwitchElement)
+
+    switchBox.appendChild(testTitle)
+    switchBox.appendChild(testSwitchContainer)
+
+    const btnLine = document.createElement('div')
+    btnLine.className = 'btn-line'
+
+    const saveBtn = document.createElement('button')
+    saveBtn.innerHTML = 'SAVE';
+    saveBtn.onclick = () => {
+        userName = (nameInputElement.value) ? nameInputElement.value : nameInputElement.placeholder;
+        cityName = (cityInputElement.value) ? cityInputElement.value : cityInputElement.placeholder;
+        let imgURL = imgInputElement.value;
+        if(imgInputElement.value)
+        document.querySelector('body').setAttribute('style',`background:url(${imgURL});background-size:cover; background-position:center;`)
+        closeDialog()
+        render()
+    }
+
+    const cancelBtn = document.createElement('button')
+    cancelBtn.innerHTML = 'CANCEL';
+    cancelBtn.onclick = () => { closeDialog() }
+
+    btnLine.appendChild(cancelBtn)
+    btnLine.appendChild(saveBtn)
+
+    dialogElement.appendChild(userNameTitle)
+    dialogElement.appendChild(nameInputElement)
+    dialogElement.appendChild(cityNameTitle)
+    dialogElement.appendChild(cityInputElement)
+    dialogElement.appendChild(imgTitle)
+    dialogElement.appendChild(imgInputElement)
+    dialogElement.appendChild(switchBox)
+    dialogElement.appendChild(btnLine)
+    dialogBox.appendChild(dialogElement)
+
+}
+
+function closeDialog() {
+    if (cityName) {
+        const setBtnElement = document.querySelector('#cog-btn')
+        setBtnElement.style = "font-size:50px";
+        setBtnElement.className = "cog-icon fa";
+        setBtnElement.innerHTML = "&#xf013";
+
+        document.querySelector('.dialog-element').setAttribute('style', 'height:0')
+        document.querySelector('.dialog-box').setAttribute('style', 'visibility:hidden')
+    }
+}
